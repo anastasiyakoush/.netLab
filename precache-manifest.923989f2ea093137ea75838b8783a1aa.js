4a8ee0188fbfe53459cb780aa1958831e1f59d8d
AnastasiyaKoush <a.koush@ya.ru>
2019-06-03 15:36:16 +0300

    Updates

---
+++ precache-manifest.923989f2ea093137ea75838b8783a1aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fdf5bf738d64a530817d3e3974310a",
+    "url": "/netLab/index.html"
+  },
+  {
+    "revision": "b74608f3dadb4ce17a2e",
+    "url": "/netLab/static/css/main.af9ca496.chunk.css"
+  },
+  {
+    "revision": "79f697b72f762237c494",
+    "url": "/netLab/static/js/2.8f7cb693.chunk.js"
+  },
+  {
+    "revision": "b74608f3dadb4ce17a2e",
+    "url": "/netLab/static/js/main.0e5b148f.chunk.js"
+  },
+  {
+    "revision": "bdf1d5acb760943a9ab7",
+    "url": "/netLab/static/js/runtime~main.9d118228.js"
+  }
+]);
