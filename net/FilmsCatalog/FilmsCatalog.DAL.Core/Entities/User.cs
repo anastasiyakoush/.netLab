1fe68aa64f56ac525f9f3b7e395fd277505f50f6
AnastasiyaKoush <a.koush@ya.ru>
2019-06-26 13:24:59 +0300

    add users migration

---
+++ net/FilmsCatalog/FilmsCatalog.DAL.Core/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FilmsCatalog.DAL.Core.Entities
+{
+    public class User : IdentityUser
+    {
+    }
+}
--- net/FilmsCatalog/FilmsCatalog.DAL/EF/FilmsCatalogContext.cs
+++ net/FilmsCatalog/FilmsCatalog.DAL/EF/FilmsCatalogContext.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using FilmsCatalog.DAL.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 namespace FilmsCatalog.DAL.EF.EF
 {
-    public class FilmsCatalogContext : DbContext
+    public class FilmsCatalogContext : IdentityDbContext<User>
     {
         public FilmsCatalogContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Film> Films { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>()
+                .Ignore(p => p.PhoneNumber)
+                .Ignore(p => p.PhoneNumberConfirmed)
+                .Ignore(p => p.TwoFactorEnabled)
+                .Ignore(p => p.AccessFailedCount)
+                .Ignore(p => p.EmailConfirmed);
+        }
     }
 }
